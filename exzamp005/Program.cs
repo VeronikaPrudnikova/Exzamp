0b25724d5d8a8173a3705f0cc7d95ee3a284fb2c
Veronika <prudnikovaveronika7@gmail.com>
2022-11-29 15:51:27 +0300

    Привет, Вероника

---
+++ exzamp005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "вероника")
+{
+    Console.WriteLine("Ура, это же ВЕРОНИКА");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
